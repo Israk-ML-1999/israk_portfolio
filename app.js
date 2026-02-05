4cad4668032927733803af5de5c0fb6d9012b487
Israk-ML-199 <hasan15-4119@diu.edu.bd>
2026-02-05 16:40:33 +0600

    add live link & theme

--- app.js
+++ app.js
@@ -151,6 +151,6 @@
 
     // Theme toggle functionality
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("dark-mode");
     })
 })();
